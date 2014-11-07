efdebd9b2adacee7430df97a5a4b596b2de801d8
BianCionaa <michelheusschen@gmail.com>
2014-11-07 22:49:44 +0100

    Create info_acp_dbtool.php
    Dutch translation of Database Optimize & Repair Tool by VSE

---
+++ language/nl/info_acp_dbtool.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* Database Optimize & Repair Tool [Dutch]
+*
+* @copyright (c) 2013 Matt Friedman
+* Dutch translation by Dutch Translators (https://github.com/dutch-translators)
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'ACP_OPTIMIZE_REPAIR'	=> 'Optimaliseer &amp; Repareer',
+	'OPTIMIZE_LOG'			=> '<strong>Database tabellen geoptimaliseerd</strong><br />» %s',
+	'REPAIR_LOG'			=> '<strong>Database tabellen gerepareerd</strong><br />» %s',
+));
