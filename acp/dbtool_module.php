e09b986932a14b599e967e863e8d02df622c0316
Matt Friedman <maf675@gmail.com>
2014-12-11 17:56:44 -0800

    Refactor acp module

--- acp/dbtool_module.php
+++ acp/dbtool_module.php
@@ -39,7 +39,7 @@ class dbtool_module
 	/** @var string */
 	public $u_action;
 
-	public function main($id, $mode)
+	public function __construct()
 	{
 		global $cache, $config, $db, $phpbb_log, $request, $template, $user;
 
@@ -52,17 +52,18 @@ class dbtool_module
 		$this->user = $user;
 
 		$this->user->add_lang_ext('vse/dbtool', 'dbtool_acp');
+	}
 
+	public function main($id, $mode)
+	{
 		$this->tpl_name = 'acp_dbtool';
 		$this->page_title = 'ACP_OPTIMIZE_REPAIR';
 
-		// Check to make sure only MySQL users can proceed
-		if ($this->db->get_sql_layer() != 'mysql4' && $this->db->get_sql_layer() != 'mysqli')
+		if (!$this->is_mysql())
 		{
 			trigger_error($this->user->lang('WARNING_MYSQL'), E_USER_WARNING);
 		}
 
-		// Get vars from the form
 		$type = $this->request->variable('type', '');
 		$marked = $this->request->variable('mark', array(''));
 		$disable_board = (!$this->config['board_disable']) ? $this->request->variable('disable_board', 0) : 0;
@@ -76,47 +77,21 @@ class dbtool_module
 					trigger_error($this->user->lang('TABLE_ERROR') . adm_back_link($this->u_action), E_USER_WARNING);
 				}
 
-				// Make sure Safe Mode is disabled during this script execution
-				if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on')
-				{
-					@ini_set('safe_mode', 'Off');
-				}
-
-				// Extend or disable script execution timeout (copied this from acp_database.php)
-				@set_time_limit(1200);
-				@set_time_limit(0);
-
+				$type = strtoupper($type);
 				$tables = implode(', ', $marked);
 				unset($marked);
 
 				switch ($type)
 				{
-					case 'optimize':
-
-						$optimize = $this->table_maintenance('OPTIMIZE TABLE', $tables, $disable_board);
-
-						$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'OPTIMIZE_LOG', time(), array($tables));
-
-						trigger_error($this->user->lang('OPTIMIZE_SUCCESS') . $optimize . adm_back_link($this->u_action));
-
-					break;
-
-					case 'repair':
-
-						$repair = $this->table_maintenance('REPAIR TABLE', $tables, $disable_board);
-
-						$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'REPAIR_LOG', time(), array($tables));
-
-						trigger_error($this->user->lang('REPAIR_SUCCESS') . $repair . adm_back_link($this->u_action));
-
-					break;
-
-					case 'check':
-
-						$check = $this->table_maintenance('CHECK TABLE', $tables, $disable_board);
-
-						trigger_error($this->user->lang('CHECK_SUCCESS') . $check . adm_back_link($this->u_action));
-
+					case 'OPTIMIZE':
+					case 'REPAIR':
+					case 'CHECK':
+						$result = $this->table_maintenance($type, $tables, $disable_board);
+						if ($type != 'CHECK')
+						{
+							$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, $type . '_LOG', time(), array($tables));
+						}
+						trigger_error($this->user->lang($type . '_SUCCESS') . $result . adm_back_link($this->u_action));
 					break;
 				}
 			}
@@ -132,29 +107,92 @@ class dbtool_module
 			}
 		}
 
-		// Generate Show Table Data
+		$this->display_tables();
+	}
+
+	/**
+	* Perform table SQL query and return any messages
+	*
+	* @param string $operation OPTIMIZE, REPAIR, or CHECK
+	* @param string $tables Comma delineated string of all tables to be processed
+	* @param int $disable_board The user's option to disable the board during run time
+	* @return string Any errors or status information
+	* @access protected
+	*/
+	protected function table_maintenance($operation, $tables, $disable_board = 0)
+	{
+		// Make sure Safe Mode is disabled during this script execution
+		if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on')
+		{
+			@ini_set('safe_mode', 'Off');
+		}
+
+		// Extend or disable script execution timeout (copied this from acp_database.php)
+		@set_time_limit(1200);
+		@set_time_limit(0);
+
+		$this->disable_board($disable_board, true);
+
+		$message = '<br />';
+		$result = $this->db->sql_query($operation . ' TABLE ' . $this->db->sql_escape($tables));
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			// Build a message only for optimize/repair errors, or if check table is run
+			if ((in_array(strtolower($row['Msg_type']), array('error', 'info', 'note', 'warning'))) || $operation == 'CHECK')
+			{
+				$message .= '<br />' . substr($row['Table'], (strpos($row['Table'], '.') + 1)) . ' ... ' . $row['Msg_type'] . ': ' . $row['Msg_text'];
+			}
+		}
+		$this->db->sql_freeresult($result);
+
+		$this->disable_board($disable_board, false);
+
+		// Clear cache to ensure board is re-enabled for all users
+		$this->cache->purge();
+
+		return $message;
+	}
+
+	/**
+	* Set disable board config state
+	*
+	* @param int $disable The users option to disable the board during run time
+	* @param bool $switch True to disable board, false to enable board
+	* @return void
+	* @access protected
+	*/
+	protected function disable_board($disable, $switch = true)
+	{
+		if ($disable)
+		{
+			$this->config->set('board_disable', (int) $switch);
+		}
+	}
+
+	/**
+	* Generate Show Table Data
+	*
+	* @return void
+	* @access protected
+	*/
+	protected function display_tables()
+	{
 		$total_data_size = $total_data_free = 0;
 
 		$tables = $this->db->sql_query('SHOW TABLE STATUS');
 
 		while ($table = $this->db->sql_fetchrow($tables))
 		{
-			/**
-			* NOTE: Only MyISAM, InnoDB and Archive storage engines can be used
-			* Check    supports MyISAM, InnoDB and Archive
-			* Optimize supports MyISAM, InnoDB and (as of MySQL 5.0.16) Archive
-			* Repair   supports MyISAM, Archive
-			*/
 			$table['Engine'] = (!empty($table['Type']) ? $table['Type'] : $table['Engine']);
-			if (in_array(strtolower($table['Engine']), array('myisam', 'innodb', 'archive')))
+			if ($this->is_valid_engine($table['Engine']))
 			{
 				// Data_free should always be 0 for InnoDB tables
-				if (strtolower($table['Engine']) == 'innodb')
+				if ($this->is_innodb($table['Engine']))
 				{
 					$table['Data_free'] = 0;
 				}
 
-				$data_size = ($table['Data_length'] + $table['Index_length']);
+				$data_size = $table['Data_length'] + $table['Index_length'];
 				$total_data_size = $total_data_size + $data_size;
 				$total_data_free = $total_data_free + $table['Data_free'];
 
@@ -163,7 +201,7 @@ class dbtool_module
 					'TABLE_TYPE'	=> $table['Engine'],
 					'DATA_SIZE'		=> $this->file_size($data_size),
 					'DATA_FREE'		=> $this->file_size($table['Data_free']),
-					'S_OVERHEAD'	=> $table['Data_free'] ? true : false,
+					'S_OVERHEAD'	=> (bool) $table['Data_free'],
 				));
 			}
 		}
@@ -177,59 +215,50 @@ class dbtool_module
 	}
 
 	/**
-	* Perform table SQL query and return any messages
+	* Only allow tables using MyISAM, InnoDB or Archive storage engines
 	*
-	* @param string $query	should either be OPTIMIZE TABLE, REPAIR TABLE, or CHECK TABLE
-	* @param string $tables comma delineated string of all tables to be processed
-	* @param int $disable_board the users option to disable the board during run time
-	* @return string $message any errors or status information
+	* @param string $engine The name of the engine
+	* @return bool True if valid engine, false otherwise
 	* @access protected
 	*/
-	protected function table_maintenance($query, $tables, $disable_board = 0)
+	protected function is_valid_engine($engine)
 	{
-		// Disable the board if admin selected this option
-		if ($disable_board)
-		{
-			$this->config->set('board_disable', 1);
-		}
+		return in_array(strtolower($engine), array('myisam', 'innodb', 'archive'));
+	}
 
-		$message = '';
-		$result = $this->db->sql_query($query . ' ' . $this->db->sql_escape($tables));
-		while ($row = $this->db->sql_fetchrow($result))
-		{
-			// Build a message only for optimize/repair errors, or if check table is run
-			if ((in_array(strtolower($row['Msg_type']), array('error', 'info', 'note', 'warning'))) || $query == 'CHECK TABLE')
-			{
-				$message .= '<br />' . substr($row['Table'], (strpos($row['Table'], '.') + 1)) . ' ... ' . $row['Msg_type'] . ': ' . $row['Msg_text'];
-			}
-		}
-		$this->db->sql_freeresult($result);
-
-		// Enable the board again if admin selected this option
-		if ($disable_board)
-		{
-			$this->config->set('board_disable', 0);
-		}
-
-		// Clear cache to ensure board is re-enabled for all users
-		$this->cache->purge();
-
-		// Let's add an extra line break if there are messages, it looks better
-		$message = !empty($message) ? '<br />' . $message : '';
-
-		return $message;
+	/**
+	* Is the storage engine InnoDB
+	*
+	* @param string $engine The name of the engine
+	* @return bool True if InnoDB engine, false otherwise
+	* @access protected
+	*/
+	protected function is_innodb($engine)
+	{
+		return strtolower($engine) == 'innodb';
 	}
 
 	/**
 	* Display file size in the proper units
 	*
-	* @param int $size number representing bytes
+	* @param int $size Number representing bytes
 	* @return string $size with the correct units symbol appended
 	* @access protected
 	*/
 	protected function file_size($size)
 	{
 		$file_size_units = array(' B', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB');
-		return $size ? round($size / pow(1024, ($i = floor(log($size) / log(1024)))), 1) . $file_size_units[(int) $i] : '0 B';
+		return ($size) ? round($size / pow(1024, ($i = floor(log($size) / log(1024)))), 1) . $file_size_units[(int) $i] : '0 B';
+	}
+
+	/**
+	* Is the database using MySQL
+	*
+	* @return bool True if MySQL, false otherwise
+	* @access protected
+	*/
+	protected function is_mysql()
+	{
+		return $this->db->get_sql_layer() == 'mysql4' || $this->db->get_sql_layer() == 'mysqli';
 	}
 }
