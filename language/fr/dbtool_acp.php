4a721617d37b3b724099b438c00a28059909c848
Raphaël <Galixte@users.noreply.github.com>
2014-12-22 01:35:55 +0100

    FRENCH translation for dbtool_acp.php
    FRENCH translation for dbtool_acp.php

--- language/fr/dbtool_acp.php
+++ language/fr/dbtool_acp.php
@@ -9,7 +9,6 @@
 * French translation by Galixte (http://www.galixte.com)
 *
 */
-
 /**
 * DO NOT CHANGE
 */
@@ -17,12 +16,10 @@ if (!defined('IN_PHPBB'))
 {
 	exit;
 }
-
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
-
 $lang = array_merge($lang, array(
 	'ACP_OPTIMIZE_REPAIR'			=> 'Optimiser &amp; réparer',
 	'ACP_OPTIMIZE_REPAIR_EXPLAIN'	=> 'Ici vous pouvez optimiser ou réparer les tables de la base de données de votre forum phpBB. Pour les bases de données importantes plusieurs minutes seront nécessaires. La fonction <strong>OPTIMISER</strong> va défragmenter votre base de données et vous offrir de meilleures performances. La fonction <strong>REPARER</strong> doit être utilisée lorsque vous avez des raisons de croire que votre base de données a rencontré une erreur ou contient des tables corrompues. Note : les tables <strong>InnoDB</strong> ne supportent pas la fonction REPARER.',
@@ -34,7 +31,7 @@ $lang = array_merge($lang, array(
 	'REPAIR'			=> 'Réparer',
 	'REPAIR_SUCCESS'	=> 'La réparation est terminée.',
 	'CHECK'				=> 'Examen',
-	'CHECK_SUCCESS'		=> 'L\'examen est terminé.<br />Si vous n\'obtenez pas comme retour : "OK" ou "Table(s) déjà à jour", vous devez exécuter une réparation.',
+	'CHECK_SUCCESS'		=> 'L\'examen est terminé.<br />Si vous n\'obtenez pas comme retour : “OK” ou “Table(s) déjà à jour”, vous devez exécuter une réparation.',
 	'WARNING'			=> 'Avertissement',
 	'WARNING_EXPLAIN'	=> 'Cet outil ne garantit pas son bon fonctionnement. Tout utilisateur de cet outil se doit de réaliser une sauvegarde de sa base de données au préalable de toute utilisation.<br /><br />Avant de poursuivre, assurez-vous d\'avoir réalisé une sauvegarde de votre base de données !',
 	'WARNING_MYSQL'		=> 'Cet fonctionnalité supporte uniquement les bases de données MySQL.',
