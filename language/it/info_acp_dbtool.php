b300f6790a28f544efc3b62767b0a95731f98893
Matt Friedman <maf675@gmail.com>
2015-07-01 22:25:40 -0700

    Add Italian translation

---
+++ language/it/info_acp_dbtool.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* Database Optimize & Repair Tool [Italian]
+*
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_OPTIMIZE_REPAIR'	=> 'Ottimizza & Ripara',
+	'OPTIMIZE_LOG'			=> '<strong>Tabelle del database ottimizzate</strong><br />» %s',
+	'REPAIR_LOG'			=> '<strong>Tabelle del database riparate</strong><br />» %s',
+));
