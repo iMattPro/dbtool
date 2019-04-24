114e736791c8a650bb5220e0dcbdda65dad8fc8f
Matt Friedman <maf675@gmail.com>
2019-04-23 20:50:05 -0700

    Add Dutch language pack

---
+++ language/nl/dbtool_acp.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* Database Optimize & Repair Tool [English]
+* Nederlandse vertaling @ Solidjeuh <https://www.muziekpromo.net>
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_OPTIMIZE_REPAIR'			=> 'Optimaliseren & repareren',
+	'ACP_OPTIMIZE_REPAIR_EXPLAIN'	=> 'Hier kunt u uw phpBB gerelateerde tabellen optimaliseren of repareren. Voor grote databases kan dit enkele minuten duren. <strong>OPTIMALISEREN</strong> zal uw database defragmenteren en kan verbeterde database prestaties bieden. <strong>REPAREREN</strong> mag alleen worden gebruikt als u reden hebt om aan te nemen dat uw database is gecrasht of corrupte tabellen bevat. Notitie: <strong>InnoDB</strong> tabellen ondersteunen geen reparatie.',
+	'OPTIMIZE_REPAIR_OPTIONS'		=> 'Database opties',
+	'DISABLE_BOARD'					=> 'Forum uitschakelen',
+	'DISABLE_BOARD_EXPLAIN'			=> 'U kunt het forum tijdens dit proces uitschakelen. Het forum wordt aan het einde van het proces ingeschakeld.',
+	'OPTIMIZE'			=> 'Optimaliseer',
+	'OPTIMIZE_SUCCESS'	=> 'Optimalisatie van de geselecteerde tabel(len) is voltooid.',
+	'REPAIR'			=> 'Herstel',
+	'REPAIR_SUCCESS'	=> 'Reparatie van de geselecteerde tabel(len) is voltooid.',
+	'CHECK'				=> 'Controleer',
+	'CHECK_SUCCESS'		=> 'Controle voltooid.<br />Als u niet “OK” krijgt, of “Tabel is al up-to-date”, moet u normaal een reparatie van de tabel uitvoeren.',
+	'WARNING'			=> 'Waarschuwing',
+	'WARNING_EXPLAIN'	=> 'Deze tool wordt geleverd ZONDER GARANTIE, en gebruikers van deze tool moeten hun volledige database back-uppen in geval van een storing.<br /><br />Controleer voordat u doorgaat of u een database back-up hebt!',
+	'WARNING_MYSQL'		=> 'Deze functie werkt alleen met MySQL databases.',
+	'MARK_OVERHEAD'		=> 'Markeer tabellen met overhead',
+	'PROCESSING'		=> 'Uw aanvraag is aan het verwerken... Even geduld...',
+	'TH_NAME'			=> 'Tabel naam',
+	'TH_TYPE'			=> 'Type',
+	'TH_SIZE'			=> 'Grootte',
+	'TH_TOTAL'			=> 'Totalen',
+	'TH_OVERHEAD'		=> 'Overhead',
+	'TABLE_ERROR'		=> 'U moet ten minste één tabel selecteren.',
+	'TABLE_EMPTY'		=> 'Tabel opslag engine niet ondersteund.',
+));
